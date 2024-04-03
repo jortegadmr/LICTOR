d8e33ef80b11efe15b8672e571b578dac69cf1db
Jao <jaortegadur@gmail.com>
2024-04-03 16:41:41 +0200

    [login]

--- elex/src/main/java/com/soltel/elex/Jwt/JwtService.java
+++ elex/src/main/java/com/soltel/elex/Jwt/JwtService.java
@@ -1,11 +1,15 @@
 package com.soltel.elex.Jwt;
 
-/* import java.util.Date; */
+import java.security.Key;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 
 
@@ -17,13 +21,17 @@ public class JwtService {
     }
 
     private String getToken(Map<String, Object> extraClaims, UserDetails user) {
-        return null;/* Jwts
+        return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
-                .compact(); */
+                .compact();
+    }
+
+    private Key getKey() {
+        return null;
     }
 }
