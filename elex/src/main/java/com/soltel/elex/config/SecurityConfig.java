a60e51f4a4c7f62aac0409c7c35c2ae8e4345c00
Jao <jaortegadur@gmail.com>
2024-04-03 17:10:43 +0200

    [+]

--- elex/src/main/java/com/soltel/elex/config/SecurityConfig.java
+++ elex/src/main/java/com/soltel/elex/config/SecurityConfig.java
@@ -2,14 +2,23 @@ package com.soltel.elex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.soltel.elex.Jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
@@ -23,7 +32,11 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults())
+                .sessionManagement(SessionManager ->
+                 SessionManager
+                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                 .authenticationProvider(authProvider)
+                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
