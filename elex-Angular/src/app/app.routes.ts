98584af7bf9286bc50fe7188fde08d008d88d008
Jao <jaortegadur@gmail.com>
2024-04-04 12:51:38 +0200

    [+] PATH

--- elex-Angular/src/app/app.routes.ts
+++ elex-Angular/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
+import { TipoExpedienteComponent } from './pages/tipo-expediente/tipo-expediente.component';
+import { ActuacionesComponent } from './pages/actuaciones/actuaciones.component';
+import { DocumentosComponent } from './pages/documentos/documentos.component';
+import { ExpedientesComponent } from './pages/expedientes/expedientes.component';
 
 export const routes: Routes = [
     /* ---Definimos las Rutas--- */
@@ -9,5 +13,11 @@ export const routes: Routes = [
     
     
     { path: 'inicio', component: DashboardComponent },
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent },
+
+    { path: 'tipo-expediente', component: TipoExpedienteComponent },
+    { path: 'actuaciones', component:ActuacionesComponent },
+    { path: 'documentos', component: DocumentosComponent },
+    { path: 'expedientes', component: ExpedientesComponent},
+
 ];
