2f6dd0feda361f9fba96558a22062a93e4be034e
Jao <jaortegadur@gmail.com>
2024-04-04 21:12:28 +0200

    ]

--- elex-Angular/src/app/app.config.ts
+++ elex-Angular/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { JwtInterceptorService } from './services/auth/jwt-interceptor.service';
+import { ErrorInterceptorService } from './services/auth/error-interceptor.service';
 
 import { routes } from './app.routes';
 
@@ -8,7 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(HttpClient, HttpClientModule),
+    importProvidersFrom(HttpClient, HttpClientModule, JwtInterceptorService, ErrorInterceptorService),
     
   ]
   
