3c6cd0834a4aee9a10fd300ea63325a593e1f982
Jao <jaortegadur@gmail.com>
2024-04-04 10:01:39 +0200

    [++++++++]

--- elex/src/main/java/com/soltel/elex/Jwt/JwtService.java
+++ elex/src/main/java/com/soltel/elex/Jwt/JwtService.java
@@ -4,10 +4,12 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -38,5 +40,31 @@ public class JwtService {
     private Key getKey() {
        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        
+        return null;
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        
+        return false;
+    }
+
+    private Claims getAllClaims(String token) 
+    {
+        return Jwts
+                .parserBuilder().
+                setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver) 
+    {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 }
