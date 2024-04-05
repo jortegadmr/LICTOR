b91552820796be12d19728a43efe04bff8efa69b
Jao <jaortegadur@gmail.com>
2024-04-05 10:51:02 +0200

    [-]

--- elex-Angular/src/app/services/tipo-expediente/tipo-expediente.service.ts
+++ elex-Angular/src/app/services/tipo-expediente/tipo-expediente.service.ts
@@ -8,35 +8,39 @@ import { Tipo } from './tipo';
 })
 export class TipoExpedienteService {
 
-  private http = inject(HttpClient);
 
-  list():Observable<Tipo>{
-    return this.http.get<Tipo>("http://localhost:8008/tipo-expediente/consultar").pipe(
-      catchError(this.handleError)
-    )
-  }
-
-  create(contact:any){
-    return this.http.post("http://localhost:8008/tipo-expediente/insertar", contact).pipe()
-  }
-
-  update(id: number, contact:any){
-    return this.http.put("http://localhost:8008/tipo-expediente/actualizar", contact).pipe()
-  }
-
-  delete (id: number){
-    return this.http.delete("http://localhost:8008/tipo-expediente/borrar/${id}").pipe()
-  }
-
-  constructor() { }
-
-  private handleError(error:HttpErrorResponse){
-    if (error.status === 0) {
-      console.error('Se ha producido un error:', error.error);
-    }else{
-      console.error('Backend retorno el codigo de estado:', error.error);
-    }
-    return throwError(() => new Error('Algo salio mal; por favor, inténtelo de nuevo.'));
-  }
 }
 
+
+// -----------------------------------------------Codigo anterior -----------------------------------------------
+
+/* private http = inject(HttpClient);
+
+list():Observable<Tipo>{
+  return this.http.get<Tipo>("http://localhost:8008/tipo-expediente/consultar").pipe(
+    catchError(this.handleError)
+  )
+}
+
+create(contact:any){
+  return this.http.post("http://localhost:8008/tipo-expediente/insertar", contact).pipe()
+}
+
+update(id: number, contact:any){
+  return this.http.put("http://localhost:8008/tipo-expediente/actualizar", contact).pipe()
+}
+
+delete (id: number){
+  return this.http.delete("http://localhost:8008/tipo-expediente/borrar/${id}").pipe()
+}
+
+constructor() { }
+
+private handleError(error:HttpErrorResponse){
+  if (error.status === 0) {
+    console.error('Se ha producido un error:', error.error);
+  }else{
+    console.error('Backend retorno el codigo de estado:', error.error);
+  }
+  return throwError(() => new Error('Algo salio mal; por favor, inténtelo de nuevo.'));
+} */
