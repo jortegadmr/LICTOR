27f7422f39888ea8a7712144c37df210821cfd0e
Jao <jaortegadur@gmail.com>
2024-04-07 10:18:42 +0200

    [+-] Antes de solucionar errores formExp

--- elex-Angular/src/app/form/expedientes-form/expedientes-form.component.ts
+++ elex-Angular/src/app/form/expedientes-form/expedientes-form.component.ts
@@ -9,6 +9,8 @@ import { Tipo } from '../../services/tipo-expediente/tipo-response';
 import { AsyncPipe } from '@angular/common';
 
 import { NgFor, NgIf, NgForOf } from '@angular/common';
+import { Expedientes } from '../../services/expedientes/expedientes-response';
+import { Condicion } from '../../services/expedientes/expedientes-response';
 @Component({
     selector: 'app-expedientes-form',
     standalone: true,
@@ -23,7 +25,8 @@ import { NgFor, NgIf, NgForOf } from '@angular/common';
         AsyncPipe,
         NgFor,
         NgIf,
-        NgForOf
+        NgForOf,
+        
         
     ]
 })
@@ -34,6 +37,8 @@ export class ExpedientesFormComponent implements OnInit {
   private router = inject(Router);
 
   public tipoExpedienteService = inject(TipoExpedienteService); //Inyeccion de dependencias, inicializa el servicio de tipo de expediente (adicional)
+
+  
 
   form = this.fb.group({
     // EL FORMULARIO
@@ -71,4 +76,58 @@ export class ExpedientesFormComponent implements OnInit {
 
 
 
+  create(){
+    if (this.form !== null ) {
+      if (this.form.valid){
+
+          // Declaración de la variable condicion
+          let condicionConst: Condicion= Condicion.Confidencial;
+
+          const condicionEnum: string | null | undefined = this.form.get('condicion')?.value;   // Asignar el valor del enum Condicion
+
+          // Mapear la cadena de texto del formulario al valor del enum Condicion
+          switch (condicionEnum) {
+            case 'confidencial':
+              condicionConst = Condicion.Confidencial;
+              break;
+            case 'en seguimiento':
+              condicionConst = Condicion.EnSeguimiento;
+              break;
+            case 'urgente':
+              condicionConst = Condicion.Urgente;
+              break;
+            default:
+          }
+
+          const expedientedato: Expedientes = { // Objeto con los datos del formulario
+              id: 0,
+              fecha: this.form.get('fecha')?.value ?? '',
+              numero: this.form.get('numero')?.value ?? '',
+              materia: this.form.get('materia')?.value ?? '',
+              estado: this.form.get('estado')?.value ?? false,
+              responsable: this.form.get('responsable')?.value ?? '',
+              responsable2: this.form.get('responsable2')?.value ?? '',
+              condicion: condicionConst, // Asignar el valor del enum Condicion
+              consejeria: this.form.get('consejeria')?.value ?? '',
+              precio: this.form.get('precio')?.value ?? 0,
+              descripcion: this.form.get('descripcion')?.value ?? '',
+              expediente: {  // Inicializa el objeto expediente dentro de expedientedato
+                id: this.form.get('expediente.id')?.value ?? 0, // Si 'id' no es un número, asigna 0 como valor predeterminado
+                nombre: this.form.get('expediente.nombre')?.value ?? '' // Si 'nombre' no es una cadena, asigna una cadena vacía como valor predeterminado
+              }
+          }
+          
+          console.log(expedientedato);
+          /* this.expedientesService.createExpediente(expedientedato) // Pasamos los datos al Servicio
+          .subscribe(()=>{
+            this.router.navigate(['expedientes']);
+            location.reload();
+          }) */
+
+      }
+      else {console.error('El formulario no es válido');}
+
+    } 
+    else {console.error('El formulario es nulo');}
+  }
 }
