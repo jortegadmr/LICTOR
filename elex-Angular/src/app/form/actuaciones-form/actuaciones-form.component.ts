8d76060c91d60394fc7310615e80314df28157dc
Jao <jaortegadur@gmail.com>
2024-04-07 13:40:24 +0200

    [+-] Calma antes de la tormaenta

--- elex-Angular/src/app/form/actuaciones-form/actuaciones-form.component.ts
+++ elex-Angular/src/app/form/actuaciones-form/actuaciones-form.component.ts
@@ -4,7 +4,7 @@ import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { AsyncPipe } from '@angular/common';
 
 import { ActuacionesService } from '../../services/actuaciones/actuaciones.service';
-import { Actuaciones } from '../../services/actuaciones/actuaciones-response';
+import { ActuacioneNombre, Actuaciones, ExpedienteNombre } from '../../services/actuaciones/actuaciones-response';
 
 import { ExpedientesService } from '../../services/expedientes/expedientes.service';  //Importar el Servicio de Expediente (TE)
 import { ExpedientesComponent } from '../../pages/expedientes/expedientes.component'; //Importar el Servicio adicional   (TE)
@@ -92,8 +92,118 @@ export class ActuacionesFormComponent implements OnInit{
     })
   })
 
+  create(){
+    if (this.form !== null ) {
+      if (this.form.valid){
+
+        // Declaración de la variable condicion
+        let nombreConst: ActuacioneNombre = ActuacioneNombre.CitacionYEmplazamiento; // ENUM del RESPONSE tienen que coincidir los nombres
+        let condicionConst: Condicion= Condicion.Confidencial; // ENUM del RESPONSE tienen que coincidir los nombres
+        let constNombre: ExpedienteNombre = ExpedienteNombre.Asi; // ENUM del RESPONSE tienen que coincidir los nombres
+
+        const nombreEnum: string | null | undefined = this.form.get('nombre')?.value;   // Asignar el valor del enum Condicion
+        const condicionEnum: string | null | undefined = this.form.get('condicion')?.value;   // Asignar el valor del enum Condicion
+        const constEnum: string | null | undefined = this.form.get('expediente.nombre')?.value;   // Asignar el valor del enum Condicion
+
+        // Mapear la cadena de texto del formulario al valor del enum Condicion
+        switch (nombreEnum) {
+          case 'Citacion y emplazamiento':
+            nombreConst = ActuacioneNombre.CitacionYEmplazamiento;
+            break;
+          case 'Declaraciones':
+            nombreConst = ActuacioneNombre.Declaraciones;
+            break;
+          case 'Informes':
+            nombreConst = ActuacioneNombre.Informes;
+            break;
+          case 'Notificaciones':
+            nombreConst = ActuacioneNombre.Notificaciones;
+            break;
+          case 'Resoluciones judiciales':
+            nombreConst = ActuacioneNombre.ResolucionesJudiciales;
+            break;
+          default:
+        }
+
+         // Mapear la cadena de texto del formulario al valor del enum Condicion IF
+         if (condicionEnum !== null && condicionEnum !== undefined) {
+          switch (condicionEnum) {
+              case 'confidencial':
+                  condicionConst = Condicion.Confidencial;
+                  break;
+              case 'en seguimiento':
+                  condicionConst = Condicion.EnSeguimiento;
+                  break;
+              case 'urgente':
+                  condicionConst = Condicion.Urgente;
+                  break;
+              default:
+                  // Manejar el caso en que el valor del formulario no coincide con ningún valor del enum
+                  break;
+            }
+          }
+
+          // Mapear la cadena de texto del formulario al valor del enum Condicion IF
+
+          if (constEnum !== null && constEnum !== undefined) {
+            switch (constEnum) {
+                case 'ASI':
+                    constNombre = ExpedienteNombre.Asi;
+                    break;
+                case 'INF':
+                    constNombre = ExpedienteNombre.Inf;
+                    break;
+                case 'JUD':
+                    constNombre = ExpedienteNombre.Jud;
+                    break;
+                case 'MO':
+                    constNombre = ExpedienteNombre.Mo;
+                    break;
+                default:
+                    // Manejar el caso en que el valor del formulario no coincide con ningún valor del enum
+                    break;
+              }
+            }
+
+
+        const actuacionDato: Actuaciones = {
+          id: 0,
+          nombre: nombreConst, // ENUM
+          fecha: this.form.get('fecha')?.value ?? '',
+          descripcion: this.form.get('descripcion')?.value ?? '',
+          estado: this.form.get('estado')?.value ?? false,
+          expediente: {
+            id: this.form.get('expediente.id')?.value ?? 0,
+            fecha: this.form.get('expediente.fecha')?.value ?? '',
+            numero: this.form.get('expediente.numero')?.value ?? '',
+            materia: this.form.get('expediente.materia')?.value ?? '',
+            estado: this.form.get('expediente.estado')?.value ?? false,
+            responsable: this.form.get('expediente.responsable')?.value ?? '',
+            responsable2: this.form.get('expediente.responsable2')?.value ?? null,
+            descripcion: this.form.get('expediente.descripcion')?.value ?? '',
+            condicion: condicionConst, // otro ENUM
+            precio: this.form.get('expediente.precio')?.value ?? 0,
+            consejeria: this.form.get('expediente.consejeria')?.value ?? '',
+            expediente: {
+              id: this.form.get('expediente.expediente.id')?.value ?? 0,
+              nombre: constNombre //  otro ENUM
+              }
+          }
+        } 
+
+        this.actuacionesService.createActuacion(actuacionDato)
+        .subscribe(() => {
+          this.router.navigate(['actuaciones']);
+          console.log("Se ha creado correctamente la Actuacion");
+          alert("Se ha creado correctamente la Actuacion");
+            location.reload();
+        })
+      }
+      else {console.error('El formulario no es válido');}
+    } 
+    else {console.error('El formulario es nulo');}
+
   
 
-
-
+  }
 }
