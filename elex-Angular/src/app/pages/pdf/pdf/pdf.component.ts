c1279b99f0f1339ad2ada4bc72d5ef1769896d42
Jao <jaortegadur@gmail.com>
2024-04-08 14:56:11 +0200

    [+]

--- elex-Angular/src/app/pages/pdf/pdf/pdf.component.ts
+++ elex-Angular/src/app/pages/pdf/pdf/pdf.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, Router, ActivatedRoute } from '@angular/router';
 import { PdfViewerModule } from 'ng2-pdf-viewer'
+
+import { AsyncPipe } from '@angular/common';
+import { DocumentosService } from '../../../services/documentos/documentos.service';
+import { Documentos } from '../../../services/documentos/documentos-response';
 
 @Component({
     selector: 'app-pdf',
@@ -10,9 +14,37 @@ import { PdfViewerModule } from 'ng2-pdf-viewer'
     CommonModule,
     RouterOutlet,
     RouterLink,
-    PdfViewerModule
+    PdfViewerModule,
+    AsyncPipe,
+
     ],
     templateUrl: './pdf.component.html',
     styleUrl: './pdf.component.css',
 })
-export class PdfComponent { }
+export class PdfComponent  { 
+    public documentService = inject (DocumentosService);
+    private route = inject(ActivatedRoute);
+
+    docuMent: Documentos[]=[]; // Guardamos los datos devueltos por el Servicio
+
+    docuMentExiste?: Documentos;
+
+    ngOnInit(): void {  // Se ejecuta al inciar el componente, guardamos los datos en la variable "document"
+        
+        const documentoId = this.route.snapshot.paramMap.get('id');
+
+        if (documentoId) {
+            this.documentService.getDocumento(parseInt(documentoId))
+            .subscribe((documentos: Documentos) => {
+                this.docuMentExiste = documentos
+            });
+
+            const nombrePDF = this.docuMentExiste?.archivo;
+        }
+      }
+
+
+
+
+
+}
